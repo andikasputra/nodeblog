e0481b920f8d0d253da3f868d93978cc1220c9cd
Andika Saputra <andika.3206@students.amikom.ac.id>
2017-06-01 09:09:11 +0700

    front index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var models = require('../models');
+var Post = models.Post;
+var Category = models.Category;
+var User = models.User;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+		Post.findAll({
+				include: [
+						{model: Category},
+						{model: User}
+				]
+		}).then(posts => {
+				console.log(posts);
+				console.log(posts[0].dataValues)
+				res.render('index', {title: 'All Posts', posts});
+		}).catch(err => {
+				console.log(err);
+		})
 });
 
 module.exports = router;
